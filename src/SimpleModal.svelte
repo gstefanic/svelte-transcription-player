94f5f5450d659dba8d57ebe7ed1f8039283e1fc8
Gregor Štefanič <gregor.stefanic@islonline.com>
2020-08-14 14:17:10 +0200

    Modal window fixed position.

--- src/SimpleModal.svelte
+++ src/SimpleModal.svelte
@@ -103,13 +103,13 @@
   }
 
   .bg {
+    position: fixed;
     z-index: 1000;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    position: absolute;
-    height: 100%;
-    width: 100%;
+    width: 100vw;
+    height: 100vh;
     background: rgba(0, 0, 0, 0.66);
   }
 
